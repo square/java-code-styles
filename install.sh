2cc81897489c6551c5fa2a52126ddb2f1623d36c
Vince Mi <vinc3m1@gmail.com>
2013-12-20 16:32:48 -0800

    update to ProlificAndroid.xml

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@ echo "Installing Square code style configs..."
 
 for i in $HOME/Library/Preferences/IntelliJIdea*/codestyles \
          $HOME/Library/Preferences/IdeaIC*/codestyles \
-         $HOME/Library/Preferences/AndroidStudio*/codestyles
+         $HOME/Library/Preferences/AndroidStudio*/codestyles 
+
 do
   cp -frv $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/configs/* $i 2> /dev/null
 done
 
 echo "Done."
 echo ""
-echo "Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'Square' or 'SquareAndroid'."
+echo "Restart AndroidStudio, go to preferences, and apply 'ProlificAndroid'."
