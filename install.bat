1c83cad5ae1f7c113fa0199db15e37c1d0c7449b
Quentin HUET <quentin.huet@epitech.eu>
2016-10-12 12:02:21 +0200

    Update with ChauffeurPrive code styles
    * TECH add cp code styles and inspection rules
    
    * TECH rebrand files with our company name
    
    * TECH rebrand install files
    
    * TECH changed company name used inside of config files

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
-REM Installs Square's IntelliJ configs into your user configs.
+REM Installs ChauffeurPrive's IntelliJ configs into your user configs.
 @echo off
-echo Installing Square IntelliJ configs...
+echo Installing ChauffeurPrive IntelliJ configs...
 
 setlocal enableDelayedExpansion
 
@@ -9,7 +9,7 @@ for /D %%i in (%userprofile%\.IdeaIC*) do call :copy_config %%i
 for /D %%i in (%userprofile%\.IntelliJIdea*) do call :copy_config %%i
 
 echo.
-echo Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'Square' or 'SquareAndroid'.
+echo Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'ChauffeurPrive' or 'ChauffeurPriveAndroid'.
 exit /b
 
 REM sub function for copy config files
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Installs Square's IntelliJ configs into your user configs.
 
-echo "Installing Square IntelliJ configs..."
+echo "Installing Chauffeur-Prive IntelliJ configs..."
 
 CONFIGS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/configs"
 
@@ -30,4 +30,4 @@ done
 
 echo "Done."
 echo ""
-echo "Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'Square' or 'SquareAndroid'."
+echo "Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'ChauffeurPrive' or 'ChauffeurPriveAndroid'."
