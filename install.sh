edfc51e801bf538d661be5484be684bbc3a786df
Peter Evjan <pevjan@squareup.com>
2020-05-21 11:09:06 +1000

    IntelliJ 2020 support
    The same config files work for IntelliJ 2020, but the location has changed
    so added that directory. Also quoting the variable derefences since the new
    path contains a space and it doesn't work without it

--- install.sh
+++ install.sh
@@ -8,23 +8,23 @@ CONFIGS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/configs"
 for i in $HOME/Library/Preferences/IntelliJIdea*  \
          $HOME/Library/Preferences/IdeaIC*        \
          $HOME/Library/Preferences/AndroidStudio* \
+         $HOME/Library/Application\ Support/JetBrains/IntelliJIdea* \
          $HOME/.IntelliJIdea*/config              \
          $HOME/.IdeaIC*/config                    \
          $HOME/.AndroidStudio*/config
 do
-  if [[ -d $i ]]; then
-
+  if [[ -d "$i" ]]; then
     # Install codestyles
-    mkdir -p $i/codestyles
-    cp -frv "$CONFIGS/codestyles"/* $i/codestyles
+    mkdir -p "$i/codestyles"
+    cp -frv "$CONFIGS/codestyles"/* "$i/codestyles"
 
     # Install inspections
-    mkdir -p $i/inspection
-    cp -frv "$CONFIGS/inspection"/* $i/inspection
+    mkdir -p "$i/inspection"
+    cp -frv "$CONFIGS/inspection"/* "$i/inspection"
 
     # Install options ("Exclude from Import and Completion")
-    mkdir -p $i/options
-    cp -frv "$CONFIGS/options"/* $i/options
+    mkdir -p "$i/options"
+    cp -frv "$CONFIGS/options"/* "$i/options"
   fi
 done
 
